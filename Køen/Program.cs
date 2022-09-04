ab8a2f361d124248c027044c526dd98c35c1a760
David F.N <99806140+Pozizion-Tango@users.noreply.github.com>
2022-09-04 23:09:32 +0200

    Add project files.

---
+++ Køen/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Køen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GUI menu = new GUI();
+            while(true)
+            {
+                menu.Menu();
+            } 
+        }
+    }
+}
